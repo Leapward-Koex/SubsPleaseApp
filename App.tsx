1d88347ddabdb9c2b218b8d31e3c4ddd79dff7a1
TecktonikMuffin <sot838@gmail.com>
2022-05-07 13:42:54 +1200

    Add persistent logging for debugging

--- App.tsx
+++ App.tsx
@@ -1,81 +1,85 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import 'react-native-gesture-handler';
-import {NavigationContainer} from '@react-navigation/native';
-import {useColorScheme} from 'react-native';
-import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { useColorScheme } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import {BottomNavBar} from './components/BottomNavBar';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { BottomNavBar } from './components/BottomNavBar';
 import nodejs from 'nodejs-mobile-react-native';
 import Toast from 'react-native-toast-message';
-import {localWebServerManager} from './services/LocalWebServerManager';
-import {LogBox} from 'react-native';
+import { localWebServerManager } from './services/LocalWebServerManager';
+import { LogBox } from 'react-native';
+import { FileLogger } from 'react-native-file-logger';
 
 LogBox.ignoreLogs(['new NativeEventEmitter']); // Ignore log notification by message
 declare global {
-  namespace ReactNativePaper {
-    interface ThemeColors {
-      secondary: string;
-      tertiary: string;
+    namespace ReactNativePaper {
+        interface ThemeColors {
+            secondary: string;
+            tertiary: string;
 
-      subsPleaseDark1: string;
-      subsPleaseDark2: string;
-      subsPleaseDark3: string;
-      darkText: string;
+            subsPleaseDark1: string;
+            subsPleaseDark2: string;
+            subsPleaseDark3: string;
+            darkText: string;
 
-      subsPleaseLight1: string;
-      subsPleaseLight2: string;
-      subsPleaseLight3: string;
-      lightText: string;
+            subsPleaseLight1: string;
+            subsPleaseLight2: string;
+            subsPleaseLight3: string;
+            lightText: string;
+        }
     }
-  }
 }
 
 const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#cb2b78',
-    secondary: '#7289da',
-    tertiary: '#09d6d6',
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: '#cb2b78',
+        secondary: '#7289da',
+        tertiary: '#09d6d6',
 
-    subsPleaseDark1: '#111111',
-    subsPleaseDark2: '#1f1f1f',
-    subsPleaseDark3: '#333333',
-    darkText: '#c2c2c2',
+        subsPleaseDark1: '#111111',
+        subsPleaseDark2: '#1f1f1f',
+        subsPleaseDark3: '#333333',
+        darkText: '#c2c2c2',
 
-    subsPleaseLight1: '#ffffff',
-    subsPleaseLight2: '#f9f9f9',
-    subsPleaseLight3: '#ebebeb',
-    lightText: '#3d3d3d',
-  },
+        subsPleaseLight1: '#ffffff',
+        subsPleaseLight2: '#f9f9f9',
+        subsPleaseLight3: '#ebebeb',
+        lightText: '#3d3d3d',
+    },
 };
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
 
-  useEffect(() => {
-    nodejs.start('main.js');
-    nodejs.channel.addListener('message', msg => {
-      if (msg.name === 'log') {
-        console.log(msg.text);
-      }
-    });
-    localWebServerManager.startServer();
-  }, []);
+    useEffect(() => {
+        (async () => {
+            nodejs.start('main.js');
+            nodejs.channel.addListener('message', (msg) => {
+                if (msg.name === 'log') {
+                    console.log(msg.text);
+                }
+            });
+            await FileLogger.configure();
+            localWebServerManager.startServer();
+        })();
+    }, []);
 
-  return (
-    <NavigationContainer>
-      <PaperProvider theme={theme}>
-        <BottomNavBar />
-        <Toast />
-      </PaperProvider>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <PaperProvider theme={theme}>
+                <BottomNavBar />
+                <Toast />
+            </PaperProvider>
+        </NavigationContainer>
+    );
 };
 
 export default App;
--- components/SettingsTab.tsx
+++ components/SettingsTab.tsx
@@ -2,79 +2,93 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import uniqBy from 'lodash.uniqby';
 import * as React from 'react';
 import {
-  BottomNavigation,
-  Text,
-  Title,
-  TouchableRipple,
-  useTheme,
+    BottomNavigation,
+    Text,
+    Title,
+    TouchableRipple,
+    useTheme,
 } from 'react-native-paper';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-import {promiseEach} from '../HelperFunctions';
-import {SubsPleaseApi} from '../SubsPleaseApi';
-import {ReleasesTab} from './ReleasesTab';
-import {WatchListTab} from './WatchListTab';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { promiseEach } from '../HelperFunctions';
+import { SubsPleaseApi } from '../SubsPleaseApi';
+import { ReleasesTab } from './ReleasesTab';
+import { WatchListTab } from './WatchListTab';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {ShowInfo} from '../models/models';
+import { ShowInfo } from '../models/models';
 import {
-  FlatList,
-  ScrollView,
-  SectionList,
-  StatusBar,
-  StyleSheet,
-  View,
+    FlatList,
+    ScrollView,
+    SectionList,
+    StatusBar,
+    StyleSheet,
+    View,
 } from 'react-native';
-import {Appbar} from 'react-native-paper';
-import {ImportExportListItem} from './settingsPageComponents/ExportImportSettings';
-import {SavedShowLocationSettings} from './settingsPageComponents/SavedShowLocationSettings';
-import {SettingsDivider} from './settingsPageComponents/SettingsDivider';
-import {Appearance} from 'react-native-appearance';
+import { Appbar } from 'react-native-paper';
+import { ImportExportListItem } from './settingsPageComponents/ExportImportSettings';
+import { SavedShowLocationSettings } from './settingsPageComponents/SavedShowLocationSettings';
+import { SettingsDivider } from './settingsPageComponents/SettingsDivider';
+import { Appearance } from 'react-native-appearance';
+import { FileLogger } from 'react-native-file-logger';
 
 export const SettingsTab = () => {
-  const {colors} = useTheme();
+    const { colors } = useTheme();
 
-  const backgroundStyle = {
-    backgroundColor:
-      Appearance.getColorScheme() === 'light'
-        ? colors.subsPleaseLight2
-        : colors.subsPleaseDark2,
-  };
+    const backgroundStyle = {
+        backgroundColor:
+            Appearance.getColorScheme() === 'light'
+                ? colors.subsPleaseLight2
+                : colors.subsPleaseDark2,
+    };
 
-  const textStyle = {
-    color:
-      Appearance.getColorScheme() === 'light'
-        ? colors.subsPleaseDark3
-        : colors.subsPleaseLight1,
-  };
+    const textStyle = {
+        color:
+            Appearance.getColorScheme() === 'light'
+                ? colors.subsPleaseDark3
+                : colors.subsPleaseLight1,
+    };
 
-  const touchableStyle = {
-    height: 60,
-    backgroundColor: colors.subsPleaseDark1,
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingLeft: 20,
-    borderRadius: 4,
-  };
+    const touchableStyle = {
+        height: 60,
+        backgroundColor: colors.subsPleaseDark1,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingLeft: 20,
+        borderRadius: 4,
+    };
 
-  return (
-    <>
-      <Appbar.Header statusBarHeight={1}>
-        <Appbar.Content title="Settings" />
-      </Appbar.Header>
-      <ScrollView style={backgroundStyle}>
-        <SavedShowLocationSettings />
-        <SettingsDivider />
-        <ImportExportListItem type="Import" />
-        <ImportExportListItem type="Export" />
-        <SettingsDivider />
-        <TouchableRipple
-          onPress={() => AsyncStorage.clear()}
-          style={touchableStyle}>
-          <View>
-            <Title style={textStyle}>Clear all data</Title>
-          </View>
-        </TouchableRipple>
-      </ScrollView>
-    </>
-  );
+    return (
+        <>
+            <Appbar.Header statusBarHeight={1}>
+                <Appbar.Content title="Settings" />
+            </Appbar.Header>
+            <ScrollView style={backgroundStyle}>
+                <SavedShowLocationSettings />
+                <SettingsDivider />
+                <ImportExportListItem type="Import" />
+                <ImportExportListItem type="Export" />
+                <SettingsDivider />
+                <TouchableRipple
+                    onPress={() => AsyncStorage.clear()}
+                    style={touchableStyle}
+                >
+                    <View>
+                        <Title style={textStyle}>Clear all data</Title>
+                    </View>
+                </TouchableRipple>
+                <TouchableRipple
+                    onPress={() =>
+                        FileLogger.sendLogFilesByEmail({
+                            subject: 'SubsPlease logs',
+                        })
+                    }
+                    style={touchableStyle}
+                >
+                    <View>
+                        <Title style={textStyle}>Send logs</Title>
+                    </View>
+                </TouchableRipple>
+            </ScrollView>
+        </>
+    );
 };
