1d5565c6ccf9f4cd6b064062efa76be8db8f3a4d
TecktonikMuffin <sot838@gmail.com>
2022-05-12 22:43:52 +1200

    Fix notification due to missing resources

--- buildAndroid.ps1
+++ buildAndroid.ps1
@@ -1,5 +1,19 @@
 npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
 cd android
-Remove-Item './app/src/main/res/drawable-*' -Recurse
+Remove-Item './app/src/main/res/drawable-hdpi/node_modules*' -Recurse
+Remove-Item './app/src/main/res/drawable-hdpi/resources*' -Recurse
+
+Remove-Item './app/src/main/res/drawable-mdpi/node_modules*' -Recurse
+Remove-Item './app/src/main/res/drawable-mdpi/resources*' -Recurse
+
+Remove-Item './app/src/main/res/drawable-xhdpi/node_modules*' -Recurse
+Remove-Item './app/src/main/res/drawable-xhdpi/resources*' -Recurse
+
+Remove-Item './app/src/main/res/drawable-xxhdpi/node_modules*' -Recurse
+Remove-Item './app/src/main/res/drawable-xxhdpi/resources*' -Recurse
+
+Remove-Item './app/src/main/res/drawable-xxxhdpi/node_modules*' -Recurse
+Remove-Item './app/src/main/res/drawable-xxxhdpi/resources*' -Recurse
+
 ./gradlew assembleRelease
 cd ..
