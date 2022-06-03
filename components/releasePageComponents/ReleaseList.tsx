4357e17abc1df84f55f139bd9a7cb6c48c423aa4
TecktonikMuffin <sot838@gmail.com>
2022-06-03 22:47:18 +1200

    Temporarily disable animated scrolling

--- components/releasePageComponents/ReleaseList.tsx
+++ components/releasePageComponents/ReleaseList.tsx
@@ -4,6 +4,7 @@ import {
     Appearance,
     StyleSheet,
     ListRenderItemInfo,
+    FlatList,
 } from 'react-native';
 import { useTheme } from 'react-native-paper';
 import { ShowInfo, WatchList } from '../../models/models';
@@ -27,12 +28,6 @@ export const ReleaseList = ({
     const scrollY = React.useRef(new Animated.Value(0)).current;
     const { colors } = useTheme();
 
-    const onFlatlistScroll = () => {
-        Animated.event([{ nativeEvent: { contentOffset: { y: scrollY } } }], {
-            useNativeDriver: true,
-        });
-    };
-
     const styles = StyleSheet.create({
         backgroundStyle: {
             backgroundColor:
@@ -43,45 +38,20 @@ export const ReleaseList = ({
     });
 
     const renderItem = ({ item, index }: ListRenderItemInfo<ShowInfo>) => {
-        const itemHeight = 150;
-        const inputRange = [
-            -1,
-            0,
-            itemHeight * index,
-            itemHeight * (index + 1.5),
-        ];
-        const opcaityInputRange = [
-            -1,
-            0,
-            itemHeight * index,
-            itemHeight * (index + 1),
-        ];
-        const translateY = scrollY.interpolate({
-            inputRange,
-            outputRange: [0, 0, 0, itemHeight],
-        });
-        const opacity = scrollY.interpolate({
-            inputRange: opcaityInputRange,
-            outputRange: [1, 1, 1, 0],
-        });
-
         return (
-            <Animated.View style={{ transform: [{ translateY }], opacity }}>
-                <ReleaseShow
-                    showInfo={item}
-                    watchList={watchList}
-                    onWatchListChanged={onWatchListChanged}
-                />
-            </Animated.View>
+            <ReleaseShow
+                showInfo={item}
+                watchList={watchList}
+                onWatchListChanged={onWatchListChanged}
+            />
         );
     };
 
     const getItemKey = (show: ShowInfo) => `${show.page}${show.episode}`;
     return (
-        <Animated.FlatList
+        <FlatList
             style={styles.backgroundStyle}
             data={showList}
-            onScroll={onFlatlistScroll}
             refreshing={refreshing}
             onRefresh={onPullToRefresh}
             renderItem={renderItem}
