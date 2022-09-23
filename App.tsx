b4928742e5b449faaeeae814b9c10b7b7d6d7a79
TecktonikMuffin <sot838@gmail.com>
2022-09-23 21:40:32 +1200

    Disable power management settings

--- App.tsx
+++ App.tsx
@@ -106,30 +106,6 @@ const App = () => {
                 { cancelable: false },
             );
         }
-
-        // 1. get info on the device and the Power Manager settings
-        const powerManagerInfo = await notifee.getPowerManagerInfo();
-        if (powerManagerInfo.activity) {
-            // 2. ask your users to adjust their settings
-            Alert.alert(
-                'Restrictions Detected',
-                'To ensure notifications are delivered, please adjust your settings to prevent the app from being killed',
-                [
-                    // 3. launch intent to navigate the user to the appropriate screen
-                    {
-                        text: 'OK, open settings',
-                        onPress: async () =>
-                            await notifee.openPowerManagerSettings(),
-                    },
-                    {
-                        text: 'Cancel',
-                        onPress: () => console.log('Cancel Pressed'),
-                        style: 'cancel',
-                    },
-                ],
-                { cancelable: false },
-            );
-        }
     };
 
     useEffect(() => {
