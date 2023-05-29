7608f502c142f3d07fb6b186af74c24d8e6843fe
TecktonikMuffin <sot838@gmail.com>
2023-05-29 20:19:42 +1200

    Remove some storage permissions

--- HelperFunctions/index.ts
+++ HelperFunctions/index.ts
@@ -209,34 +209,5 @@ export async function requestStoragePermission() {
         });
     };
 
-    const pm0 = await checkManageFilesPermission();
-    if (!pm0) {
-        return false;
-    }
-
-    const pm1 = await PermissionsAndroid.check(
-        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-    );
-    const pm2 = await PermissionsAndroid.check(
-        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-    );
-
-    if (pm1 && pm2) {
-        return true;
-    }
-
-    const userResponse = await PermissionsAndroid.requestMultiple([
-        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-    ]);
-
-    if (
-        userResponse['android.permission.READ_EXTERNAL_STORAGE'] ===
-            'granted' &&
-        userResponse['android.permission.WRITE_EXTERNAL_STORAGE'] === 'granted'
-    ) {
-        return true;
-    } else {
-        return false;
-    }
+    return await checkManageFilesPermission();
 }
